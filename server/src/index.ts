3e4c74f5cbb7e411cf043213b0116aeaa2d31ff8
bones-bones <elliot.ma.brown@gmail.com>
2022-01-17 21:12:27 -0800

    serving server code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,38 @@
 import { Gpio } from 'onoff';
 import { WebSocket } from 'ws';
 
+import express from 'express';
+
 const SOCKET_PORT = 8080;
 
 console.log('server up');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+    res.header('Cross-Origin-Embedder-Policy', 'require-corp');
+    res.header('Cross-Origin-Opener-Policy', 'same-origin');
+    next();
+});
+
+app.listen(port, () => {
+    return console.log(`server is listening on ${port}`);
+});
+
+app.get('/', (_req, res) => {
+    res.sendFile('./build/index.html', { root: __dirname + '/' });
+});
+
+app.get('*.js', (req, res) => {
+    res.sendFile('./build/' + req.url, {
+        root: __dirname + '/',
+    });
+});
+
+app.listen(port, () => {
+    return console.log(`server is listening on ${port}`);
+});
 
 // This is pretty gross, having a promise hang on this long is less than ideal. Fix later.
 const initializeSocketConnection = () => {
